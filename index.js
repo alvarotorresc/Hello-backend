b6fdd3818b2dce4f4b33dfa5cb041448bf62d152
Alvaro Torres <alvarotorrescarrasco@gmail.com>
2022-09-14 00:29:52 +0200

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors"
+import mongoose from 'mongoose'
+import dotenv from 'dotenv';
+
+
+const app = express();
+dotenv.config()
+
+app.use(cors())
+app.use(express.json())
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server started on ${process.env.PORT}`)
+);
+
